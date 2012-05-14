772ceacc8b4d8f172f4e5e376c8ead63a0b1722b
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-05-14 20:44:03 +0200

    Excluded files from packaging + Moved .puppet-lintrc

--- tasks/rake/lib/packaging/base_packager.rb
+++ tasks/rake/lib/packaging/base_packager.rb
@@ -44,9 +44,10 @@ class BasePackager
     url = "https://github.com/cegeka/puppet-#{module_name}"
     description = "Puppet module: #{module_name} by Cegeka\nModule #{module_name} description goes here."
 
-    static_arguments = ["-t", @package_type, "-s", "dir", "-x", ".git", "-x", ".gitignore", "-x", "build", "-x", "Rakefile", "-a", @architecture, "-m", "Cegeka <computing@cegeka.be>", "--prefix", "/etc/puppet/modules"]
+    static_arguments = ["-t", @package_type, "-s", "dir", "-a", @architecture, "-m", "Cegeka <computing@cegeka.be>", "--prefix", "/etc/puppet/modules"]
+		exclude_arguments = ["-x", ".git", "-x", ".gitignore", "-x", "tasks", "-x", "Rakefile", "-x", "target", "-x", ".project", "-x", ".puppet-lintrc"]
     var_arguments = ["-n", package_name, "-v", @semver_version, "--iteration", @release, "--url", url, "--description", description, "-C", @basedirectory, module_name]
-    arguments = static_arguments + var_arguments
+    arguments = static_arguments + exclude_arguments + var_arguments
     
     tmpdir = Dir.mktmpdir
     Dir.chdir tmpdir
