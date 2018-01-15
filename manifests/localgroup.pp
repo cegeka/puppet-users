878372fc54d29968db399fdaaf00e9e7de20ffa5
andreias <andrei.asan@cegeka.com>
2018-01-15 16:23:55 +0100

    fix localgourp + fix vagrant and 039kidapp01

--- manifests/localgroup.pp
+++ manifests/localgroup.pp
@@ -37,17 +37,10 @@ define users::localgroup ($gid, $ensure='present') {
     fail("Users::Localgroup[${title}]:
       namevar must be alphanumeric")
   }
-  if $gid.is_bool{
-    if bool2str($gid) !~ /^[0-9]+$/ {
+  if  ! is_integer($gid) and $gid !~ /^[0-9]+$/ {
       fail("Users::Localgroup[${title}]:
         parameter gid must be numeric")
     }
-  } elsif $gid.is_string {
-    if $gid !~ /^[0-9]+$/ {
-      fail("Users::Localgroup[${title}]:
-        parameter gid must be numeric")
-    }
-  }
   if $ensure in [ present, absent ] {
     $ensure_real = $ensure
   }
